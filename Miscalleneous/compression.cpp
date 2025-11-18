757c2a8174da735d86e4770f2d3f3997199d5221
Ashrafuzzaman Arnob <ashraf674127@gmail.com>
2025-11-18 18:32:08 +0600

    Refactor compression logic to use vector and STL

--- Miscalleneous/compression.cpp
+++ Miscalleneous/compression.cpp
@@ -1,5 +1,5 @@
-int v[N]; // N=total number of elements
-int C=0; // available index for new value
-v[++C]=val // compression is 1 based
-sort(v+1,v+1+C); // compression done
-lower_bound(v+1,v+1+C,val)-v // find compressed value
+vector<ll>v;
+v.push_back(val);
+sort(all(v));
+Unique(v);
+lower_bound(all(v),val)-v.begin(); // 0-based search
