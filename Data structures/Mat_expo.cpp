fdc9b68410a691f0fbb2a6fddc365fb92405a236
Ashrafuzzaman Arnob <ashraf674127@gmail.com>
2025-11-18 17:38:21 +0600

    Refactor matrix operations for cleaner syntax

--- Data structures/Mat_expo.cpp
+++ Data structures/Mat_expo.cpp
@@ -13,42 +13,33 @@ struct Matrix{
 	}
 	inline void make_unit(){
 		assert(n==m);
-		for (int i=0;i<n;i++){
-			for (int j=0;j<m;j++){
+		for (int i=0;i<n;i++)
+			for (int j=0;j<m;j++) 
 				a[i][j]=(i==j);
-			}
-		}
 	}
 	inline Matrix operator +(const Matrix &b){
 		assert(n==b.n && m==b.m);
 		Matrix ans=Matrix(n,m);
-		for (int i=0;i<n;i++){
-			for (int j=0;j<m;j++){
+		for (int i=0;i<n;i++)
+			for (int j=0;j<m;j++) 
 				ans.a[i][j]=(a[i][j]+b.a[i][j])%M;
-			}
-		}
 		return ans;
 	}
 	inline Matrix operator -(const Matrix &b){
 		assert(n==b.n && m==b.m);
 		Matrix ans=Matrix(n,m);
-		for (int i=0;i<n;i++){
-			for (int j=0;j<m;j++){
+		for (int i=0;i<n;i++)
+			for (int j=0;j<m;j++) 
 				ans.a[i][j]=(a[i][j]-b.a[i][j]+M)%M;
-			}
-		}
 		return ans;
 	}
 	inline Matrix operator *(const Matrix &b) {
 		assert(m==b.n);
 		Matrix ans=Matrix(n,b.m);
-		for (int i=0;i<n;i++){
-			for (int j=0;j<b.m;j++){
-				for (int k=0;k<m;k++){
+		for (int i=0;i<n;i++)
+			for (int j=0;j<b.m;j++)
+				for (int k=0;k<m;k++)
 					ans.a[i][j]=(ans.a[i][j]+(a[i][k]*b.a[k][j])%M)%M;
-				}
-			}
-		}
 		return ans;
 	}
 	inline Matrix pow(ll k) {
