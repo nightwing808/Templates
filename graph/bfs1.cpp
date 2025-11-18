fd13283d5bed2709c611d3543007a9e03a99f88b
Ashrafuzzaman Arnob <ashraf674127@gmail.com>
2025-11-18 18:47:30 +0600

    Update bfs1.cpp

--- graph/bfs1.cpp
+++ graph/bfs1.cpp
@@ -1,15 +1,18 @@
-int vis[N];
+int vis[N],dist[N];
 vector<int>g[N];
 void bfs(int s){
     queue<int>q;
     q.push(s);
+    vis[s]=1;
+    dist[s]=0;
     while (!q.empty()){
         int v=q.front();
         q.pop();
-        vis[v]=1;
         for (auto c:g[v]){
             if (vis[c]) continue;
             q.push(c);
+            vis[c]=1;
+            dist[c]=dist[v]+1;
         }
     }
 }
