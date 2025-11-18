26e1c258964bceac78e0e69525727540cda33c29
Ashrafuzzaman Arnob <ashraf674127@gmail.com>
2025-11-18 18:02:32 +0600

    Refactor update and query functions in sqrt.cpp

--- Data structures/sqrt.cpp
+++ Data structures/sqrt.cpp
@@ -8,16 +8,15 @@ void build(){
 		sort(b+i,b+min(n,i+sqsz));
 	}
 }
-void update(int l,int r,int k,int p){
-	a[p]=(ll)(u*k)/(r-l+1);
-	int block=p/sqsz;
+void update(int pos,int k){
+	a[pos]=k;
+	int block=pos/sqsz;
 	for (int j=block*sqsz;j<min(n,(block+1)*sqsz);j++){
 		b[j]=a[j];
 	}
 	sort(b+(block*sqsz),b+min(n,(block+1)*sqsz));
 }
-int query(int l,int r,ll v)
-{
+int query(int l,int r,ll v){
 	int st=min(r+1,((l+sqsz-1)/sqsz)*sqsz),ed=st;
 	int ans=0;
 	for (int j=st;j+sqsz-1<=r;j+=sqsz){
